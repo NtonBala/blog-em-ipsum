b27262e51fcc1074f6f09f56cf2ef7d74ce33a4c
Anton Balashov <balashov.anton.1983@gmail.com>
2024-11-24 21:36:23 +0200

    Query posts data

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { fetchPosts, deletePost, updatePost } from './api';
+import { PostDetail } from './PostDetail';
 
-import { fetchPosts, deletePost, updatePost } from "./api";
-import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
 export function Posts() {
@@ -9,31 +10,37 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const { data } = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts,
+  });
+
+  if (!data) return <div />;
 
   return (
     <>
       <ul>
         {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
+          <li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
             {post.title}
           </li>
         ))}
       </ul>
+
       <div className="pages">
         <button disabled onClick={() => {}}>
           Previous page
         </button>
+
         <span>Page {currentPage + 1}</span>
+
         <button disabled onClick={() => {}}>
           Next page
         </button>
       </div>
+
       <hr />
+
       {selectedPost && <PostDetail post={selectedPost} />}
     </>
   );
