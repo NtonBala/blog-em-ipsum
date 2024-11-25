63365b400befcc868a4de78b73c954382926e712
Anton Balashov <balashov.anton.1983@gmail.com>
2024-11-25 22:12:41 +0200

    Clean up posts query error simulation

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
 export async function fetchPosts(pageNum = 1) {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`);
 
-  throw new Error('you cannot have this data');
-
   return response.json();
 }
 
