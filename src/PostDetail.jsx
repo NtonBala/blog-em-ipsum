ecbe47bc4eebe10757da3fc0b77f857abcb58890
Anton Balashov <balashov.anton.1983@gmail.com>
2025-01-09 18:04:44 +0200

    CQ-2.1: fetch comments

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,21 +1,33 @@
-import { fetchComments } from "./api";
-import "./PostDetail.css";
+import { useQuery } from '@tanstack/react-query';
+import { fetchComments } from './api';
+import './PostDetail.css';
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: [`comments-${post.id}`],
+    queryFn: () => fetchComments(post.id),
+  });
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
-        <li key={comment.id}>
-          {comment.email}: {comment.body}
-        </li>
-      ))}
+      {isLoading && <h5>Loading...</h5>}
+      {isError && (
+        <>
+          <h5>Oops, something went wrong</h5>
+          <p>{error.toString()}</p>
+        </>
+      )}
+      {data &&
+        !isError &&
+        data.map((comment) => (
+          <li key={comment.id}>
+            {comment.email}: {comment.body}
+          </li>
+        ))}
     </>
   );
 }
